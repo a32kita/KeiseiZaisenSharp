4cba0a0c44585f2ef178d0eb4177daaffb2fa090
a32kita <a32kita@yahoo.co.jp>
2024-08-23 17:43:58 +0900

    wip

--- src/DemoAndTests/KeiseiZaisenSharp.DemoAndTests.Demo001/Program.cs
+++ src/DemoAndTests/KeiseiZaisenSharp.DemoAndTests.Demo001/Program.cs
@@ -14,6 +14,18 @@ namespace KeiseiZaisenSharp.DemoAndTests.Demo001
         {
             var trains = await kzSv.GetAllTrainsAsync();
             foreach (var train in trains)
+                PrintTrainInfo(train);
+
+            Console.WriteLine("青砥駅周辺の列車");
+            foreach (var train in trains.Where(item => item.Location.Description.Contains("青砥")))
+                PrintTrainInfo(train, false);
+
+            Console.ReadLine();
+        }
+
+        static void PrintTrainInfo(KeiseiZaisenTrain train, bool detail = true)
+        {
+            if (detail)
             {
                 Console.WriteLine("* ==============");
                 Console.WriteLine("  {0} {1}行き", train.TrainType, train.Destination);
@@ -22,105 +34,10 @@ namespace KeiseiZaisenSharp.DemoAndTests.Demo001
                 Console.WriteLine("  ==============");
                 Console.WriteLine();
             }
-
-            Console.ReadLine();
-        }
-
-        static async Task MainProc(KeiseiZaisenService kzSv)
-        {
-            var trafficInfo = await kzSv.GetRawTrafficInfoAsync();
-            var stations = await kzSv.GetRawStationsAsync();
-            var ikisakis = await kzSv.GetRawIkisakisAsync();
-            var stops = await kzSv.GetRawStopsAsync();
-            var syasyus = await kzSv.GetRawSyasyusAsync();
-
-            var enameConverter = (string s) =>
+            else
             {
-                // 入力値 E001 など
-                foreach (var st in stops.Stop)
-                {
-                    var codeInt = 0;
-                    if (!Int32.TryParse(st.Code, out codeInt))
-                        continue;
-
-                    if (s == $"E{codeInt.ToString("000")}")
-                        return st.Name;
-
-                    if (s == $"D{codeInt.ToString("000")}")
-                        return st.Name + "周辺 (下り)";
-
-                    if (s == $"U{codeInt.ToString("000")}")
-                        return st.Name + "周辺 (上り)";
-                }
-
-                return "不明";
-            };
-
-            var syasyuConverter = (string s) =>
-            {
-                var rs = syasyus.Syasyu.SingleOrDefault(item => item.Code == s);
-
-                return rs?.Name ?? "不明";
-            };
-
-            var hokoConverter = (string s) =>
-            {
-                if (s == "0")
-                    return "上り";
-                if (s == "1")
-                    return "下り";
-
-                return "不明";
-            };
-
-            var alphOrder = (char c) =>
-            {
-                return c switch
-                {
-                    'D' => 0,  // D が最初
-                    'E' => 1,  // E が次
-                    'U' => 2,  // U が最後
-                    _ => 3     // その他は想定外
-                };
-            };
-
-            var trainInfos = new List<TrafficSection>();
-            trainInfos.AddRange(trafficInfo.TS);
-            trainInfos.AddRange(trafficInfo.EK);
-            trainInfos.Sort((x, y) =>
-            {
-                // 数値部分を比較
-                int numberX = int.Parse(x.Id.Substring(1)); // 文字列の2文字目以降を数値に変換
-                int numberY = int.Parse(y.Id.Substring(1));
-                int result = numberX.CompareTo(numberY);
-
-                // 数値が同じ場合、アルファベット部分をカスタム順序で比較
-                if (result == 0)
-                {
-                    result = alphOrder(x.Id[0]).CompareTo(alphOrder(y.Id[0]));
-                }
-
-                return result;
-            });
-
-            foreach (var trEnt in trainInfos)
-            {
-                var stop = enameConverter(trEnt.Id);
-
-                Console.WriteLine("ID = {0} ({1})", trEnt.Id, stop);
-                foreach (var tr in trEnt.Tr)
-                {
-                    var hoko = hokoConverter(tr.Hk);
-                    var syasyu = syasyuConverter(tr.Sy);
-                    var ikisaki = ikisakis.Ikisaki.SingleOrDefault(item => item.Code == tr.Ik)?.Name ?? "不明";
-                    
-                    Console.WriteLine("　・({0}) {1} {2}行き ({3})", hoko, syasyu, ikisaki, tr.No);
-                }
-
-                Console.WriteLine();
+                Console.WriteLine("  {0} {1}行き ({2})", train.TrainType, train.Destination, train.TrainNumber);
             }
-
-            Console.WriteLine();
         }
     }
 }
--- src/KeiseiZaisenSharp/KeiseiZaisenTrainLocation.cs
+++ src/KeiseiZaisenSharp/KeiseiZaisenTrainLocation.cs
@@ -21,22 +21,22 @@ namespace KeiseiZaisenSharp
                         continue;
 
                     if (this.RawSource.Id == $"E{codeInt.ToString("000")}")
-                        return "[停車中] " + st.Name;
+                        return $"[停車中] {st.Name}";
 
                     if (this.RawSource.Id == $"D{codeInt.ToString("000")}")
                     {
                         var prevStp = this._configurationSources.GetNextStop(st, 0);
-                        return "[走行中, 下り] " + prevStp?.Name + " => " + st.Name;
+                        return $"[走行中, 下り] {prevStp?.Name} => {st.Name}";
                     }
 
                     if (this.RawSource.Id == $"U{codeInt.ToString("000")}")
                     {
                         var prevStp = this._configurationSources.GetNextStop(st, 1);
-                        return "[走行中, 上り] " + prevStp?.Name + " => " + st.Name;
+                        return $"[走行中, 上り] {prevStp?.Name} => {st.Name}";
                     }
                 }
 
-                return "不明";
+                return "[不明]";
             }
         }
 
